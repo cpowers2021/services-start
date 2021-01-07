2e5ff49563bb8025cec0704d41c328f06c7593b7
BuildTools <unconfigured@null.spigotmc.org>
2021-01-06 16:49:34 -0800

    Feat: Log services into account and new account using the constructor and providers

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,24 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService] //tells how to construct service
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  constructor(private loggingService: LoggingService) //creates a property and binds it;
+  //add a type assignment for class that needs to be injected
+   {
 
+  }
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
