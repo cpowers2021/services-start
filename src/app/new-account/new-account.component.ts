18c1e554f363cf46dfb2a1cd0e5ea97017f00365
BuildTools <unconfigured@null.spigotmc.org>
2021-01-07 21:57:37 -0800

    Feat: Create an alert whenever a status update arrives

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,18 +6,18 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService] //tells how to construct service
+  //providers: [LoggingService] //tells how to construct service
 })
 export class NewAccountComponent {
   constructor(private loggingService: LoggingService, private accountsService: AccountsService)
   //creates a property and binds it;
   //add a type assignment for class that needs to be injected
-
    {
-
+    this.accountsService.subscribe((status:string) => alert('New Status: ' + status));
+    //alert is a popup. subscribe shows how to create an alert?
   }
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    //this.loggingService.logStatusChange(accountStatus);
   }
 }
