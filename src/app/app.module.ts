26fe9adffdf431b4e3f3c72314e6116b5b7c2665
BuildTools <unconfigured@null.spigotmc.org>
2021-01-07 21:53:46 -0800

    Feat: Add the LoggingService at the top of the hierarchical injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AccountsService } from './accounts.service';
+import { LoggingService } from './logging.service';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
@@ -18,7 +19,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
